16a98583c7e2fab5a97812602c72e8a3c086bba2
Yinon Bar <yinonbar1988@gmail.com>
2022-12-31 18:44:09 +0200

    basic layout

---
+++ src/Comps/LayoutArea/Layout/Layout.js
@@ -0,0 +1,16 @@
+import "./Layout.css";
+
+import React from "react";
+
+function Layout() {
+  return (
+    <div className="Layout">
+      <header>Header</header>
+      <aside>Aside</aside>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default Layout;
