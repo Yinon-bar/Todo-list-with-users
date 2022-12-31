672a8cf48b0368d0289148c81baffffda0170300
Yinon Bar <yinonbar1988@gmail.com>
2022-12-31 19:00:28 +0200

    basic layout

--- src/Comps/LayoutArea/Layout/Layout.js
+++ src/Comps/LayoutArea/Layout/Layout.js
@@ -1,11 +1,13 @@
 import "./Layout.css";
-
 import React from "react";
+import Header from "../Header/Header";
 
 function Layout() {
   return (
     <div className="Layout">
-      <header>Header</header>
+      <header>
+        <Header />
+      </header>
       <aside>Aside</aside>
       <main>Main</main>
       <footer>Footer</footer>
