5450f35c9c9983b5e41a8432876cb6c3ec90d757
Yinon bar <yinonbar1988@gmail.com>
2023-01-02 09:26:56 +0200

    NavBar

--- src/Comps/LayoutArea/Aside/Aside.js
+++ src/Comps/LayoutArea/Aside/Aside.js
@@ -1,7 +1,13 @@
 import "./Aside.css";
 
 function Aside() {
-  return <aside className="Aside">Aside</aside>;
+  return (
+    <aside className="Aside">
+      <a href="">Home</a>
+      <a href="">Todo List</a>
+      <a href="">Users</a>
+    </aside>
+  );
 }
 
 export default Aside;
--- src/Comps/LayoutArea/Main/Main.js
+++ src/Comps/LayoutArea/Main/Main.js
@@ -3,15 +3,17 @@ import TaskCounter from "../../Dashboard/TaskCounter/TaskCounter";
 import Tasks from "../../Dashboard/TasksList/TasksList";
 import Welcome from "../../Dashboard/Welcome/Welcome";
 import Users from "../../Users/UsersList/UsersList";
+import Routing from "../Routing/Routing";
 import "./Main.css";
 
 function Main() {
   return (
     <main className="Main">
-      <Welcome />
-      <TaskCounter />
-      <Tasks />
-      <Users />
+      <Routing />
+      {/* <Welcome /> */}
+      {/* <TaskCounter /> */}
+      {/* <Tasks /> */}
+      {/* <Users /> */}
     </main>
   );
 }
