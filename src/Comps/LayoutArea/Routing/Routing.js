f4a26b6955658a37d65f18f3b369055f5c853b6b
Yinon bar <yinonbar1988@gmail.com>
2023-01-02 14:04:15 +0200

    posts

--- src/Comps/LayoutArea/Routing/Routing.js
+++ src/Comps/LayoutArea/Routing/Routing.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../../Pages/Home/Home";
 import Page404 from "../../Pages/Page404/Page404";
+import Posts from "../../Pages/Posts/Posts";
 import TodoList from "../../Pages/TodoList/TodoList";
 import Users from "../../Pages/Users/Users";
 
@@ -11,6 +12,7 @@ function Routing() {
       <Route path="/home" element={<Home />} />
       <Route path="/todo" element={<TodoList />} />
       <Route path="/users" element={<Users />} />
+      <Route path="/posts" element={<Posts />} />
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
